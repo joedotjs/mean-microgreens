688a3872557988dcc0dc3849116bae9d23c627aa
Ben Neiswander <ben.neiswander@gmail.com>
2015-06-09 18:22:00 -0400

    set up ajax REST routes for: blends, micros, orders, reviews

---
+++ server/app/routes/micros/index.js
@@ -0,0 +1,40 @@
+var router = require('express').Router();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var micros = mongoose.model('Micros');
+
+module.exports = router;
+
+//Get all micros or specific micro
+router.get('/', function (req, res, next){
+	micros.find({"_id": req.query.microid}, function(err, micros){
+		res.json(micros);
+	});
+}); 
+
+// we need to build admin only posting routes 
+router.post('/', function (req, res, next){
+	var micro = new micros(req.body);
+	micro.save(function(err){
+		res.status(200).send(micro);
+	});
+});
+
+router.put('/:microid', function (req, res, next){
+	micros.findOne({"_id": req.params.microid}, function(err, micro){
+		for(var key in req.body){
+			micro[key] = req.body[key];
+		}
+		micro.save(function(err){
+			res.status(200).send(micro);
+		});
+	});
+});
+
+router.delete('/:microid', function (req, res, next){
+	micros.findOne({"_id": req.params.microid}, function(err, micro){
+		micro.remove(function(err){
+			res.status(204).send();
+		});
+	});
+});
