f6228cddca1721906dd17c5e8c44d16b5dc6f0ad
Thomas Proenza <tomproenza@Thomass-MacBook-Pro.local>
2015-06-11 13:10:50 -0400

    added some auth to order routes

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -9,17 +9,53 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('AboutController', function ($scope, FullstackPics, AjaxFactory) {
+app.controller('AboutController', function ($scope, FullstackPics, MicrosFactory) {
 
     // Images of beautiful Fullstack people.
     $scope.images = _.shuffle(FullstackPics);
     $scope.micros;
+    $scope.newMicro = {
+        name: "kitten",
+        spice: "mild",
+        price: 10,
+        description: "soooo cutteeeee!!!",
+        image: 'http://cdn.cutestpaw.com/wp-content/uploads/2011/11/cute-cat-l.jpg',
+        inventory: 1
+        };
 
-    $scope.getAllMicros = function () {
-    	AjaxFactory.getMicros().then(function (micros) {
-    		$scope.micros = micros;
-    	});
-    }
+    $scope.showAllMicros = function () {
+        MicrosFactory.getAllMicros().then(function (micros) {
+            $scope.micros = micros;
+        });
+    };
+    $scope.showMicroById = function(microid) {
+        MicrosFactory.getMicroById(microid).then(function (micro){
+            $scope.micros = micro;
+        });
+    };
+    $scope.showMicroByName = function(microname) {
+        MicrosFactory.getMicroByName(microname).then(function (micro){
+            $scope.micros = micro;
+        });
+    };
+    $scope.addMicro = function (micro) {
+        console.log("in add micro");
+        MicrosFactory.createMicro(micro).then(function (newMicro){
+            $scope.newMicro = {
+                name: null,
+                spice: null,
+                price: null,
+                description: null,
+                image: null,
+                inventory: null
+            };
+        });
+    };
+    $scope.deleteMicro = function (id){
+        MicrosFactory.deleteMicroById(id).then(function(){
+            return;
+        });
+    };
 
 
 });
