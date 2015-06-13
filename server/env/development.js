69cf02c4a2a95178c1f1c3950e7ca060ef0070a2
Thomas Proenza <tomproenza@Thomass-MacBook-Pro.local>
2015-06-13 18:25:45 -0400

    finished with oauth for fb and google, need to fix twitter

--- server/env/development.js
+++ server/env/development.js
@@ -2,14 +2,14 @@ module.exports = {
   "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
-    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
-    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
-    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
+    "consumerKey": "IAwYKh3ly6sdHEqNAlIYWDCR9v",
+    "consumerSecret": "vRMLMyEV1Xn2hqgsRfC4MfHpbbwRYEg8KDl3zlAQ5XlbI55CLi",
+    "callbackUrl": "http://127.0.0.1:1337/auth/twitter/callback"
   },
   "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
+    "clientID": "849259408482825",
+    "clientSecret": "b98fa4ebba68ccc03ec5200940603cb8",
+    "callbackURL": "http://localhost:1337/auth/facebook/callback"
   },
   "GOOGLE": {
     "clientID": "735156327397-4eg868a6794hl5n7v5vb127jqesplfpk.apps.googleusercontent.com",
